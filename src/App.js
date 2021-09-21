c7317cea35a716791912643cf52287ae1d7d264a
Motoharu SOMEYA <s.motoharu@wano.co.jp>
2021-09-21 10:16:12 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react'
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = useState({idname:undefined})
+
+    useEffect(() => {
+        (async () => {
+            window.setTimeout( // 5秒後に setState を呼び出す
+                setState,
+                5*1000,
+                {idname:{id:10, name:"name"}} // これを渡している
+            )
+        })()
+    })
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <RenderNumber number={state.idname?.id}></RenderNumber>
+            </header>
+        </div>
+    );
 }
+
+function RenderNumber(props){
+    const number = props.number
+
+    return (<>
+        <button variant="contained" color="primary" onClick={()=>onclick(number)}>button</button>
+    </>)
+}
+
+function onclick(num) {
+    window.alert("number : " + num)
+}
+
 
 export default App;
